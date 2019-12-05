4c252b8322968ade4b60d94f83400dd93ee3a6d2
Luís Eduardo Lucca Bassan <luccabassan@gmail.com>
2019-12-05 21:32:38 -0200

    Classe central telefônica

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -1,11 +1,18 @@
 package Controlador;
 
+import Modelo.CentralTelefonica;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		System.out.println("Projeto Central Telefônica");
-		
+
+		CentralTelefonica ct1 = new CentralTelefonica();
+		CentralTelefonica ct2 = new CentralTelefonica();
+
+		System.out.println(CentralTelefonica.getTipoRamal()[2]);
+
 	}
 
 }
--- src/Modelo/CentralTelefonica.java
+++ src/Modelo/CentralTelefonica.java
@@ -4,11 +4,12 @@ public class CentralTelefonica {
 
 	private String modelo; // nome da central
 
-	private int tipoRamal[] = { 0, 1, 2 }; // "analogico", "ip", "digital"
-	private int qtdadeRamal[] = { 0, 0, 0 };
+	private static int tipoRamal[] = { 0, 1, 2 }; // "analogico", "ip", "digital"
 
-	private int tipoTronco[] = { 0, 1, 2 }; // "analogico", "ip", "digital"
-	private int qtdadeTronco[] = { 0, 0, 0 };
+	private int qtdadeRamal[]; // atribuição das qtdades { 60, 0, 100 };
+
+	private static int tipoTronco[] = { 0, 1, 2 }; // "analogico", "ip", "digital"
+	private int qtdadeTronco[]; // atribuição das qtdades { 10, 0, 0 };
 
 	private Categoria categoria; // tipo da central, se é híbrida ou ip
 
@@ -23,6 +24,152 @@ public class CentralTelefonica {
 	private Capacidade capacidadeRamalIP;
 
 	private Placa placa;
+
+	public CentralTelefonica() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CentralTelefonica(String modelo, int[] qtdadeRamal, int[] qtdadeTronco, Categoria categoria, Disa disa,
+			float preco, Capacidade capacidadeLinhaAnalogica, Capacidade capacidadeLinhaDigital,
+			Capacidade capacidadeLinhaIP, Capacidade capacidadeRamalAnalogico, Capacidade capacidadeRamalDigital,
+			Capacidade capacidadeRamalIP, Placa placa) {
+		super();
+		this.modelo = modelo;
+		this.qtdadeRamal = qtdadeRamal;
+		this.qtdadeTronco = qtdadeTronco;
+		this.categoria = categoria;
+		this.disa = disa;
+		this.preco = preco;
+		this.capacidadeLinhaAnalogica = capacidadeLinhaAnalogica;
+		this.capacidadeLinhaDigital = capacidadeLinhaDigital;
+		this.capacidadeLinhaIP = capacidadeLinhaIP;
+		this.capacidadeRamalAnalogico = capacidadeRamalAnalogico;
+		this.capacidadeRamalDigital = capacidadeRamalDigital;
+		this.capacidadeRamalIP = capacidadeRamalIP;
+		this.placa = placa;
+
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public static int[] getTipoRamal() {
+		return tipoRamal;
+	}
+
+	public static void setTipoRamal(int[] tipoRamal) {
+		CentralTelefonica.tipoRamal = tipoRamal;
+	}
+
+	public int[] getQtdadeRamal() {
+		return qtdadeRamal;
+	}
+
+	public void setQtdadeRamal(int[] qtdadeRamal) {
+		this.qtdadeRamal = qtdadeRamal;
+	}
+
+	public static int[] getTipoTronco() {
+		return tipoTronco;
+	}
+
+	public static void setTipoTronco(int[] tipoTronco) {
+		CentralTelefonica.tipoTronco = tipoTronco;
+	}
+
+	public int[] getQtdadeTronco() {
+		return qtdadeTronco;
+	}
+
+	public void setQtdadeTronco(int[] qtdadeTronco) {
+		this.qtdadeTronco = qtdadeTronco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Disa getDisa() {
+		return disa;
+	}
+
+	public void setDisa(Disa disa) {
+		this.disa = disa;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public Capacidade getCapacidadeLinhaAnalogica() {
+		return capacidadeLinhaAnalogica;
+	}
+
+	public void setCapacidadeLinhaAnalogica(Capacidade capacidadeLinhaAnalogica) {
+		this.capacidadeLinhaAnalogica = capacidadeLinhaAnalogica;
+	}
+
+	public Capacidade getCapacidadeLinhaDigital() {
+		return capacidadeLinhaDigital;
+	}
+
+	public void setCapacidadeLinhaDigital(Capacidade capacidadeLinhaDigital) {
+		this.capacidadeLinhaDigital = capacidadeLinhaDigital;
+	}
+
+	public Capacidade getCapacidadeLinhaIP() {
+		return capacidadeLinhaIP;
+	}
+
+	public void setCapacidadeLinhaIP(Capacidade capacidadeLinhaIP) {
+		this.capacidadeLinhaIP = capacidadeLinhaIP;
+	}
+
+	public Capacidade getCapacidadeRamalAnalogico() {
+		return capacidadeRamalAnalogico;
+	}
+
+	public void setCapacidadeRamalAnalogico(Capacidade capacidadeRamalAnalogico) {
+		this.capacidadeRamalAnalogico = capacidadeRamalAnalogico;
+	}
+
+	public Capacidade getCapacidadeRamalDigital() {
+		return capacidadeRamalDigital;
+	}
+
+	public void setCapacidadeRamalDigital(Capacidade capacidadeRamalDigital) {
+		this.capacidadeRamalDigital = capacidadeRamalDigital;
+	}
+
+	public Capacidade getCapacidadeRamalIP() {
+		return capacidadeRamalIP;
+	}
+
+	public void setCapacidadeRamalIP(Capacidade capacidadeRamalIP) {
+		this.capacidadeRamalIP = capacidadeRamalIP;
+	}
+
+	public Placa getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(Placa placa) {
+		this.placa = placa;
+	}
 
 	/*
 	 * Modelo de necessidade: 100 ramais IP 60 ramais analógicos 60 linhas/tronco
